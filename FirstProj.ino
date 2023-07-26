b4b40f83b334a3154af3c3b64b5484d5084dee37
LivingJW30 <118578583+LivingJW30@users.noreply.github.com>
2023-07-26 11:59:27 -0400

    Arduino code updated with working alram

--- FirstProj.ino
+++ FirstProj.ino
@@ -1,51 +1,64 @@
-int trigPin = 9;    // TRIG pin
-int echoPin = 8; // ECHO pin
-int ledpin = 7; //LED pin
-float threshold = 112.00;
-int buzzpin = 6;
+//pin values 
+const int trigPin = 9;
+const int echoPin = 8;
+const int ledpin = 7;
+const int buzzpin = 6;
 
-float duration_us, distance_cm;
+# define THRES 112.00
 
 void setup() {
-  // begin serial port
+  //begin serial port
   Serial.begin (9600);
 
-  // configure the trigger pin to output mode
+  //config pins to respective Input/Output mode 
   pinMode(trigPin, OUTPUT);
-  // configure the echo pin to input mode
   pinMode(echoPin, INPUT);
-  // config the LED
-  pinMode(ledpin,OUTPUT);
-  pinMode(buzzpin,OUTPUT);
+  pinMode(ledpin, OUTPUT);
+  pinMode(buzzpin, OUTPUT);
 }
-//print fucntion using pass by reference
-void print(float* distance_cm){
+
+//alarm function using pass by reference
+void alarm(const float* const distance_cm){
+  if(*distance_cm < THRES){
+    for(int i = 0;i<100;i++){
+      digitalWrite(buzzPin, HIGH);
+      digitalWrite(ledPin, HIGH);
+      delay(1000);
+      digitalWrite(buzzPin, LOW);
+      digitalWrite(ledPin, LOW);
+      delay(1000);
+    }
+  }
+  else{
+    digitalWrite(buzzPin, LOW);
+    digitalWrite(ledPin, LOW);
+  }
+}
+
+//print to serial monitor function using pass by reference
+void print(const float* const distance_cm){
   Serial.print("distance: ");
   Serial.print(*distance_cm);
   Serial.println(" cm");
 }
 
 void loop() {
-  // generate 10-microsecond pulse to TRIG pin
+  //defining floats here saves roughly 1% of storage (3888 to 3816 bytes)
+  float duration_us, distance_cm;
+
+  //10-microsecond pulse generated to TRIG pin
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
 
-  // measure duration of pulse from ECHO pin
+  //measure duration of pulse from ECHO pin
   duration_us = pulseIn(echoPin, HIGH);
 
-  // calculate the distance
+  //calculate distance
   distance_cm = 0.017 * duration_us;
   
-  if(distance_cm < threshold){
-    digitalWrite(buzzpin,HIGH);
-    digitalWrite(ledpin,HIGH);
-    delay(1000);
-  }
-  else{
-    digitalWrite(buzzpin,LOW);
-    digitalWrite(ledpin,LOW);
-  }
+  //call to the alarm
+  alarm(&distance_cm);
 
   //call to print
   print(&distance_cm);
